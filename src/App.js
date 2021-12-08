3ebc1d15ae851d0c1f0ce829efb07b0e328f158c
vishakha0112 <89861784+vishakha0112@users.noreply.github.com>
2021-12-09 02:37:01 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import './App.css';
+
+import {Counter} from './container/counter';
+const App=()=>{
+  console.log('App render');
+  return(
+    <Counter/>
+  )
+}
+export default App;
